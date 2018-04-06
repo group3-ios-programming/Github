1e7ff38f99e1f5acb3211fc6acfc9220112f0d0c
Cntt32 <Admin32@Cntt32s-iMac.local>
2018-04-06 10:18:54 +0700

    Initial Commit

---
+++ aaaa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  aaaa
+//
+//  Created by Cntt32 on 4/6/18.
+//  Copyright © 2018 com.group08. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
